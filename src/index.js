e62df836ce5983ed55edd67fea4075ce41240c30
崔海峰 <cuihaifeng1919@hotmail.com>
2018-03-15 19:49:27 +0800

    更新基本规则

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-
+import Vue from 'vue';
 import viewportUnitsBuggyfill from 'viewport-units-buggyfill';
 import viewportUnitsBuggyfillHacks from 'viewport-units-buggyfill/viewport-units-buggyfill.hacks.js';
 import fastclick from 'fastclick';
 //  解决实例化的对象方法polyfill(例: [1,2,3].includes)
 import 'babel-polyfill';
+import '@/styles/common.styl';
 
 //  处理vw单位兼容性
 window.onload = () => {
@@ -14,3 +15,6 @@ window.onload = () => {
   fastclick.attach(document.body);
 };
 
+new Vue({
+  id: 'app'
+}).$mount('#app');
